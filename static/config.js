509807a25d13bf51175f5bd05fc52e48f800ae70
Andrey Weber <Andrey.Weber@gmail.com>
2016-12-01 12:04:03 +0100

    Initial adding marksboard demo pages and scripts

---
+++ static/config.js
@@ -0,0 +1,7 @@
+var APP_CONFIG = {
+    apiKey:         'AIzaSyDu8kDrHdk0QEFCYe0LZn4rm7phXZ9tJ7g',
+    clientId:       '326560008785-g3iungt3ffj1lk2dpv67001ati8rgmm8.apps.googleusercontent.com',
+    spreadsheetId:  '1EP4c-9UZVkEb4n4eM2fU5JYuBYZxUAuXXeppDzV0rjg',
+    discoveryUrl:   'https://sheets.googleapis.com/$discovery/rest?version=v4',
+    scope:          'https://www.googleapis.com/auth/spreadsheets.readonly'
+};
