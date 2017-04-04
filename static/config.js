7ee80564cb31a69bcd0afc28e844d785eb372f5f
Alexey Zaslavsky <pterolex@gmail.com>
2017-04-04 19:48:35 +0300

    Update spreadsheet id

--- static/config.js
+++ static/config.js
@@ -1,7 +1,7 @@
 var APP_CONFIG = {
     apiKey:         'AIzaSyDu8kDrHdk0QEFCYe0LZn4rm7phXZ9tJ7g',
     clientId:       '935103078791-0hec5n3cb9dp2h35nbn76nq4nqlaaihb.apps.googleusercontent.com',
-    spreadsheetId:  '1EP4c-9UZVkEb4n4eM2fU5JYuBYZxUAuXXeppDzV0rjg',
+    spreadsheetId:  '1JWdRY9MOo6SXeSHb92huOf8q-jz0odnSXKjfp0lfyqM',
     discoveryUrl:   'https://sheets.googleapis.com/$discovery/rest?version=v4',
     scope:          'https://www.googleapis.com/auth/spreadsheets.readonly'
 };
