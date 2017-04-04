3b0cde35c408ef96f5f3c7e44fd1b2ffaec2ff31
Alexey Zaslavsky <pterolex@gmail.com>
2017-04-04 20:09:02 +0300

    Update API key

--- static/config.js
+++ static/config.js
@@ -1,5 +1,5 @@
 var APP_CONFIG = {
-    apiKey:         'AIzaSyDu8kDrHdk0QEFCYe0LZn4rm7phXZ9tJ7g',
+    apiKey:         'AIzaSyCVx3j_7OlMtvE1XCZdFOHKPdBjpmG6uWc',
     clientId:       '935103078791-0hec5n3cb9dp2h35nbn76nq4nqlaaihb.apps.googleusercontent.com',
     spreadsheetId:  '1JWdRY9MOo6SXeSHb92huOf8q-jz0odnSXKjfp0lfyqM',
     discoveryUrl:   'https://sheets.googleapis.com/$discovery/rest?version=v4',
