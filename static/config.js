3ced20e395f80e14918cfce2f201f0282a7ad09f
Alexey Zaslavsky <pterolex@users.noreply.github.com>
2017-04-04 19:47:02 +0300

    Update config.js

--- static/config.js
+++ static/config.js
@@ -1,6 +1,6 @@
 var APP_CONFIG = {
     apiKey:         'AIzaSyDu8kDrHdk0QEFCYe0LZn4rm7phXZ9tJ7g',
-    clientId:       '326560008785-g3iungt3ffj1lk2dpv67001ati8rgmm8.apps.googleusercontent.com',
+    clientId:       '935103078791-0hec5n3cb9dp2h35nbn76nq4nqlaaihb.apps.googleusercontent.com',
     spreadsheetId:  '1EP4c-9UZVkEb4n4eM2fU5JYuBYZxUAuXXeppDzV0rjg',
     discoveryUrl:   'https://sheets.googleapis.com/$discovery/rest?version=v4',
     scope:          'https://www.googleapis.com/auth/spreadsheets.readonly'
